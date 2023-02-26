1df0864a34a5741f2adf35141bd656c405fa1dfe
Lucas Miranda <lucasrezende12@hotmail.com>
2023-02-26 15:09:42 -0300

    New structure, Node, HTTPS

---
+++ src/run.js
@@ -0,0 +1,38 @@
+const fs                    = require('fs');
+const https                 = require('https');
+const path                  = require('path');
+const express               = require('express');
+const helmet                = require('helmet')
+const redirectToHTTPS       = require('express-http-to-https').redirectToHTTPS
+const routerContent         = require("./routers/routerContent")
+
+//Dirs
+const publicDirectory = path.join(__dirname, "../public")
+const sslDirectory = process.env.SSLDIR || path.join(__dirname, "../ssl");
+
+//Certbot stuff (HTTPS)
+const httpsOptions = {
+  key: fs.readFileSync(sslDirectory + '/privkey.pem'),
+  cert: fs.readFileSync(sslDirectory + '/cert.pem'),
+};
+
+//Setup express
+const httpsport = process.env.PORT_HTTPS;
+const httpport  = process.env.PORT_HTTP;
+const exp = express();
+const server = https.createServer(httpsOptions, exp)
+
+//middleware
+exp.use(redirectToHTTPS([], [], 301));
+exp.use(helmet())
+
+//Routers
+exp.use(routerContent)
+
+//Listen to http and redirect to https
+exp.listen(httpport);
+
+//Listen to https
+server.listen(httpsport, () => {
+    console.log("Server is up!")
+});
